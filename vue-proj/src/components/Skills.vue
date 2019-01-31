bb56070e5e6497006c6cc9b8e1ce6ce0b84437e6
sunbx <sunbenxin@p1.com>
2019-01-31 16:50:21 +0800

    Init project

---
+++ vue-proj/src/components/Skills.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="skills">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>{{index}}. {{data.skill}}</li>
+      </ul>
+    </div>
+    <div v-bind:class="{ alert: showAlert}">,W</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data(){
+    return {
+      skills:[
+        {"skill":"Vue.js"},
+        {"skill":"Frontend Developer"}
+      ],
+      showAlert: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.alert {
+  background-color: green;
+  width:100%;
+  height:30px;
+}
+</style>
